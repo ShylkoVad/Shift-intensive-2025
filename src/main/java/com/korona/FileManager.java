d615c077a38cc38a479ec5c1cc678fea34852d99
ShylkoVad <shylkovad@gmail.com>
2025-08-19 12:15:48 +0300

    добавил методы clearErrorLog и printDepartmentManagerID

--- src/main/java/com/korona/FileManager.java
+++ src/main/java/com/korona/FileManager.java
@@ -1,7 +1,9 @@
 package com.korona;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FileManager {
 
@@ -14,6 +16,19 @@ public class FileManager {
             }
         } catch (IOException e) {
             System.err.println("Error creating file for department manager ID " + departmentManagerID + ": " + e.getMessage());
+        }
+    }
+
+    // Метод для вывода departmentManagerID на экран
+    public void printDepartmentManagerID(String departmentManagerID) {
+        System.out.println("Department Manager ID: " + departmentManagerID);
+    }
+
+    public void clearErrorLog() {
+        try (PrintWriter out = new PrintWriter(new FileWriter(Constants.ERROR_LOG, false))) { // false для перезаписи
+            // Просто создаем пустой файл
+        } catch (IOException e) {
+            System.err.println("Error clearing error log: " + e.getMessage());
         }
     }
 }
