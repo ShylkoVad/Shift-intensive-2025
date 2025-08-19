ea239935c20af33bcbd6b83b3ed78a862fe3de96
ShylkoVad <shylkovad@gmail.com>
2025-08-19 12:56:45 +0300

    добавил перезапись информации если в нем есть уже информация

--- src/main/java/com/korona/FileManager.java
+++ src/main/java/com/korona/FileManager.java
@@ -10,9 +10,16 @@ public class FileManager {
     public void createDepartmentFile(String departmentManagerID) {
         File file = new File(departmentManagerID + ".sb"); // Создаем файл с расширением .sb
         try {
+            // Создаем новый файл, если он не существует
             if (!file.exists()) {
-                file.createNewFile(); // Создаем новый файл, если он не существует
+                file.createNewFile();
                 System.out.println("File created: " + file.getName());
+            } else {
+                // Если файл существует, очищаем его содержимое
+                try (PrintWriter writer = new PrintWriter(new FileWriter(file, false))) {
+                    // Очищаем файл, просто открыв его с параметром false
+                    System.out.println("File exists. Contents will be overwritten: " + file.getName());
+                }
             }
         } catch (IOException e) {
             System.err.println("Error creating file for department manager ID " + departmentManagerID + ": " + e.getMessage());
