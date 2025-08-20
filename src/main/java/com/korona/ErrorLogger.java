d8d546727a5a41e6ed391e3d930c37f844174891
ShylkoVad <shylkovad@gmail.com>
2025-08-20 11:32:30 +0300

    обработка ошибок вынесена в отдельный класс

---
+++ src/main/java/com/korona/ErrorLogger.java
@@ -0,0 +1,32 @@
+package com.korona;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ErrorLogger {
+    private static ErrorLogger instance;
+    private String logFilePath;
+
+    private ErrorLogger(String logFilePath) {
+        this.logFilePath = logFilePath;
+    }
+
+    public static synchronized ErrorLogger getInstance(String logFilePath) {
+        if (instance == null) {
+            instance = new ErrorLogger(logFilePath);
+        }
+        return instance;
+    }
+
+    public void logError(String message) {
+        try (FileWriter fw = new FileWriter(logFilePath, true); // true для добавления в конец файла
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(message); // Записываем сообщение об ошибке
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
+    }
+}
